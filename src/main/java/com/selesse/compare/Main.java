e2a18fc26374ac57f3ebf4bf2bffd5e789214436
Alex Selesse <alex@selesse.com>
2019-08-14 08:36:28 -0400

    Add lazy CLI options to support both diff modes

--- src/main/java/com/selesse/compare/Main.java
+++ src/main/java/com/selesse/compare/Main.java
@@ -1,11 +1,8 @@
 package com.selesse.compare;
 
-import com.selesse.compare.filesystem.DirectoryRoot;
 import com.selesse.compare.filesystem.comparison.Diff;
 import com.selesse.compare.filesystem.comparison.DiffFactory;
-import com.selesse.compare.filesystem.comparison.FileContentDiff;
 
-import java.io.File;
 import java.io.IOException;
 
 public class Main {
@@ -13,24 +10,7 @@ public class Main {
         if (args.length < 1) {
             throw new RuntimeException("Expected arguments, got " + args.length);
         }
-        // TODO: smarter argument input
-        if (args.length == 1) {
-            Diff diff = new FileContentDiff(DirectoryRoot.fromPath(new File(args[0]).toPath()));
-            diff.printOutput();
-        } else {
-            var firstDirectory = args[0];
-            var secondDirectory = args[1];
-
-            var firstPath = new File(firstDirectory).toPath();
-            var secondPath = new File(secondDirectory).toPath();
-
-            var firstRoot = DirectoryRoot.fromPath(firstPath);
-            var secondRoot = DirectoryRoot.fromPath(secondPath);
-
-            System.out.printf("%s => %d directories, %d files%n", firstPath, firstRoot.getDirectories().size(), firstRoot.getFiles().size());
-            System.out.printf("%s => %d directories, %d files%n", secondPath, secondRoot.getDirectories().size(), secondRoot.getFiles().size());
-            var diff = DiffFactory.create(firstRoot, secondRoot);
-            diff.printOutput();
-        }
+        Diff diff = DiffFactory.create(args);
+        diff.printOutput();
     }
 }
--- src/main/java/com/selesse/compare/filesystem/comparison/DiffFactory.java
+++ src/main/java/com/selesse/compare/filesystem/comparison/DiffFactory.java
@@ -2,11 +2,46 @@ package com.selesse.compare.filesystem.comparison;
 
 import com.selesse.compare.filesystem.DirectoryRoot;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class DiffFactory {
-    public static Diff create(DirectoryRoot rootA, DirectoryRoot rootB) {
-        if (System.getenv().getOrDefault("DIFF_STRATEGY", "").equals("file")) {
-            return new FileContentDiff(rootA, rootB);
+    public static Diff create(String[] args) throws IOException {
+        if (Arrays.asList(args).contains("--type=file")) {
+            List<String> directories =
+                    Stream.of(args).filter(arg -> !arg.equals("--type=file")).collect(Collectors.toList());
+            return new FileContentDiff(directories.stream()
+                    .map(dir -> {
+                        try {
+                            return DirectoryRoot.fromPath(new File(dir).toPath());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).toArray(DirectoryRoot[]::new));
+        } else if (Arrays.asList(args).contains("--type=directory")) {
+            List<String> directories =
+                    Stream.of(args).filter(arg -> !arg.equals("--type=directory")).collect(Collectors.toList());
+            if (directories.size() != 2) {
+                throw new RuntimeException("Expected 2 directories to compare, got " + directories.size());
+            }
+            var firstDirectory = directories.get(0);
+            var secondDirectory = directories.get(1);
+
+            var firstPath = new File(firstDirectory).toPath();
+            var secondPath = new File(secondDirectory).toPath();
+
+            var firstRoot = DirectoryRoot.fromPath(firstPath);
+            var secondRoot = DirectoryRoot.fromPath(secondPath);
+
+            System.out.printf("%s => %d directories, %d files%n", firstPath, firstRoot.getDirectories().size(), firstRoot.getFiles().size());
+            System.out.printf("%s => %d directories, %d files%n", secondPath, secondRoot.getDirectories().size(), secondRoot.getFiles().size());
+            return new StructuredFolderDiff(firstRoot, secondRoot);
+        } else {
+            throw new RuntimeException("Unspecified diff type, try --type=file or --type=directory");
         }
-        return new StructuredFolderDiff(rootA, rootB);
     }
 }
